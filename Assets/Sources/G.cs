451a154b30df004ff50fc575919ab5f9090fc2b0
Vanson Wing Leung <vanson@vanportdev.com>
2025-07-05 10:16:56 +0800

    test OK

--- Assets/Sources/G.cs
+++ Assets/Sources/G.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using fs_minicar_control_lib_win10;
 
 public class G : MonoBehaviour
 {
     UdpCommunication udpComm;
+
+    public Slider uiInputX;
+    public Slider uiInputY;
+
+
+    string connectedAddress = null;
+
+
+    // Rate limiting variables
+    private float timeSinceLastSend = 0f;
+    private const float sendInterval = 1f / 30f; // 30 FPS
+
 
     void Start()
     {
@@ -13,6 +26,27 @@ public class G : MonoBehaviour
         Debug.Log("3 + 4 = " + d);
 
         udpComm = new UdpCommunication();
+        udpComm.OnPrintLog += message => Debug.Log(message);
+        udpComm.OnReceiveCommandPacket += (packet) =>
+        {
+            // Log the received command packet
+            Debug.Log($"Just Received command packet: {packet}");
+
+            if (packet.Command.Equals("DISCOVERY"))
+            {
+                Debug.Log($"DISCOVERY Received");
+                connectedAddress = packet.Address;
+            }
+            else if (packet.Command.Equals("ACK"))
+            {
+                Debug.Log($"ACK Received");
+            }
+            else
+            {
+                Debug.Log($"packet.Command: {packet.Command} DISCOVERY");
+                Debug.Log($"packet.Command  {packet.Command.Equals("DISCOVERY")}");
+            }
+        };
 
         // Start listening for packets
         _ = udpComm.ListenForPacketsAsync();
@@ -25,11 +59,30 @@ public class G : MonoBehaviour
 
     void Update()
     {
+        timeSinceLastSend += Time.deltaTime; // Accumulate time since last send
 
+        if (connectedAddress != null && timeSinceLastSend >= sendInterval)
+        {
+            // Reset the timer
+            timeSinceLastSend = 0f;
+
+            int inputX = (int)uiInputX.value;
+            int inputY = (int)uiInputY.value;
+            byte[] responseData = {
+                (byte)inputX, // X input
+                (byte)inputY  // Y input
+            };
+
+            udpComm.SendPacket(
+                connectedAddress,
+                responseData
+            );
+        }
     }
+
 
     void OnApplicationQuit()
     {
         udpComm.Close();
-    }    
+    }
 }
